daa571a539ea353bbc95ade9abfd3b3788f9e6ab
Natanael <natanael.gloryant2020@gmail.com>
2023-10-17 09:46:45 +0700

    ...

--- Semester4/TekOp/sederhana.py
+++
@@ -1,4 +0,0 @@
-dj, mj, sj, tj = [float(x) for x in input().split()]
-daur_ulang = int(dj*mj if sj*tj>=dj*mj else sj*tj)
-kayu_segar = int(dj-daur_ulang)
-print("Kertas daur ulang: ", daur_ulang,"\nKertas dari kayu segar: ", kayu_segar)
